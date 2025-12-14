0a3430867a1ecd1edb7b505de5d602f6affa0163
ratneshranve <ratneshranve@gmail.com>
2025-12-15 03:06:27 +0530

    update authmiddleware

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-
+import authMiddleware from "../middleware/auth.middleware.js";
 import { getMe, setActiveSemester } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/me", getMe);
-router.put("/active-semester", setActiveSemester);
+router.get("/me", authMiddleware, getMe);
+router.put("/active-semester", authMiddleware, setActiveSemester);
 
 export default router;
